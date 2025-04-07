f942d9f0767a0add055e8b077cdd1213c1f8b8dd
Dan Horner <dan.horner@fishtownanalytics.com>
2025-04-07 17:24:18 +0000

    changed fct_order_items to an incremental model for illustrative purposes

--- models/marts/finance/fct_order_items.sql
+++ models/marts/finance/fct_order_items.sql
@@ -1,18 +1,17 @@
-{{
-    config(
-        materialized='incremental',
-        unique_key='order_item_id'
+{{ config(
 
-    )
+    materialized='incremental',
+    unique_key = 'order_item_id' 
+
+)
 }}
-
 with order_item as (
     
     select * from {{ ref('int_order_items_joined') }}
     {% if is_incremental() %}
-        where commit_date > (select max(commit_date) from {{ this }} )
+        where commit_date > (select dateadd('day', -3, max(commit_date)) from {{ this }} )
     {% endif %}
-
+    
 ),
 part_supplier as (
     
